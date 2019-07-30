7764b6620209711ebc4c103a472ccbd857fa6710
SpencerHann <47734610+SpencerHann@users.noreply.github.com>
2019-07-30 11:04:34 -0400

    added README.sh

---
+++ TESTCASES/README.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Run this script to see an example execution of the BLAST-QC program.
+#The output of each execution is placed into a separate subdirectory of the TESTCASES directory.
+
+#Most basic call- filename and type of BLAST (in this case blastx) run are required parameters.
+#will result in 3 files: one header with the <Query_def> of the matching hits, one with info on the hits that did not 
+#match input (in this case none) and one with info on hits that match the input arguments. 
+python ../BLAST-QC.py --filename sampleResults.xml --version x --output one.out/example_one.out --order e 
+
+#replicating a `-max_target_seqs` parameter (without all the confusion) note the low level of taxonomy detail.
+python ../BLAST-QC.py -f sampleResults.xml -v x -o two.out/example_two.out --number 1 -or e
+
+#There! using a range value makes finding a good hit with acceptable taxonomy info easier.
+python ../BLAST-QC.py -f sampleResults.xml -v x -o three.out/example_three.out -n 2 --erange .0005 -or e
+
+#Thresholds for lots of important values.
+python ../BLAST-QC.py -f sampleResults.xml -v x -o four.out/example_four.out -n 1 -or e --evalue .03 --bitscore 35 --taxonomy 1 --identity 60
+
